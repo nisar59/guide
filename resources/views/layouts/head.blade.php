0354c2e3d83aae9814d2d74165136942f36c3dd0
Umair Awan <umair.awan@akhuwat.org.pk>
2022-11-22 18:48:31 +0500

    commit

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,6 +1,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
 
     <title>{{Settings()->portal_name}}-@yield('title')</title>
   <link rel="stylesheet" href="{{asset('assets/css/app.min.css')}}">
