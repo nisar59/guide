cbeec66ac0a606a986025bb42e30a2b3aa7c2c3f
Umair Awan <umair.awan@akhuwat.org.pk>
2022-11-22 10:50:18 +0500

    commit

--- resources/views/frontend/include/head.blade.php
+++ resources/views/frontend/include/head.blade.php
@@ -5,7 +5,7 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
 
-	<title></title>
+	<title>{{Settings()->portal_name}}</title>
 <style type="text/css">
 .dropdown-toggle::after{
 	content: none;
